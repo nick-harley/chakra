4424692c434c231726c214f6ccd1292844b07150
Nicholas Harley <nick.j.harley@gmail.com>
2021-09-10 15:01:00 +0100

    function to caculate prob from model

--- ContextModels.jl
+++ ContextModels.jl
@@ -9,16 +9,26 @@ function construct_model(s::Vector{T} where T<:Chakra.Obj,v::T where T<:Viewpoin
 
     vs = reverse(vp_map(v,s))
     n = length(vs)
+
+    db[[]] = n
     
     for h in 1:order+1
-        println(h)
         for i in 1:n-h+1
-            seq = vs[i:i+h-1]
+            seq = reverse(vs[i:i+h-1])
             haskey(db,seq) ? db[seq]+=1 : db[seq] = 1
         end
     end
     return db
-    
+
+end
+
+function probability_of(next,context,model)
+
+    ctx_count = model[context]
+    seq_count = model[[next,context...]]
+
+    return seq_count/ctx_count
+
 end
 
 
