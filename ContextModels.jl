8116a8b2972dcba8b808b44ba9f7b359a5714ebf
Nicholas Harley <nick.j.harley@gmail.com>
2021-09-13 12:15:22 +0100

    type of context models

--- ContextModels.jl
+++ ContextModels.jl
@@ -2,6 +2,13 @@ module ContextModels
 
 using Typeside, Chakra, Viewpoints
 
+struct ContextModel{T}
+    db::Dict{Vector{T},Int}
+    elems::Set{T}
+end
+
+get_count(c::ContextModel,s::Vector) = haskey(c.db,s) ? c.db[s] : 0
+
 function construct_model(s::Vector{T} where T<:Chakra.Obj,v::T where T<:Viewpoint,order::Int)
 
     tau = vp_type(v)
@@ -11,6 +18,7 @@ function construct_model(s::Vector{T} where T<:Chakra.Obj,v::T where T<:Viewpoin
     n = length(vs)
 
     db[[]] = n
+    elems = Set(vs)
     
     for h in 1:order+1
         for i in 1:n-h+1
@@ -18,19 +26,22 @@ function construct_model(s::Vector{T} where T<:Chakra.Obj,v::T where T<:Viewpoin
             haskey(db,seq) ? db[seq]+=1 : db[seq] = 1
         end
     end
-    return db
+    return ContextModel{tau}(db,elems)
 
 end
 
-function probability_of(next,context,model)
-
-    ctx_count = model[context]
-    seq_count = model[[next,context...]]
+function probability_of(next::T,context::Vector{T},model::ContextModel{S}) where {S,T<:S} 
+    ctx_count = get_count(model,context)
+    seq_count = get_count(model,[next,context...])
 
     return seq_count/ctx_count
 
 end
 
-
+probability_of(next::T,model::ContextModel{S}) where {T,S>:T} = probability_of(next,T[],model)
 
 end
+
+
+
+
--- MidiPitch.jl
+++ MidiPitch.jl
@@ -16,5 +16,8 @@ end
 diff(x::NoteNumber,y::NoteNumber)::NoteInterval = NoteInterval(y.val-x.val)
 shift(i::NoteInterval,p::NoteNumber)::NoteNumber = NoteNumber(p.val+i.val)
 
+Base.convert(::Type{NoteNumber},n::Int64) = NoteNumber(n)
+Base.convert(::Type{Int64},n::NoteNumber) = n.val
+
 end
 
